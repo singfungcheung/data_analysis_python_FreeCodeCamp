fbe65a04324fc5e310bb44222cb1bd12fad7ea87
Cheung <cheunsi@ad.vzwcorp.com>
2024-04-04 14:29:25 -0400

    started numpy exercise

--- src/numpy_intro.py
+++ src/numpy_intro.py
@@ -1,5 +1,25 @@
 import numpy as np
 
 a = np.array([1, 2, 3, 4])
-print(a >= 2)
-print(a[a >= 2])
+# print(a >= 2)
+# print(a[a >= 2])
+
+"""
+Exercises
+"""
+
+# Version of Python
+print("Python version: " + np.__version__)
+
+# Create a numpy array of size 10, filled with zeros.
+print(np.zeros(10))
+
+# Create a numpy array with values randing from 10 to 49
+print(np.arange(10, 50))
+
+# Create a numpy matrix of 2x2, filled with ones.
+print(np.ones([2, 2], dtype=np.int8))
+
+# Create a numpy matrix of 3*2 float numbers, filled with ones.
+print(np.ones([3, 2], dtype=np.float16))
+
