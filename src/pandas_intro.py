742b4f133d5f3b8bb173c8ca4784e5efd1923549
singfungcheung <singfung.cheung@gmail.com>
2024-04-07 13:32:43 -0400

    started pandas exercieses

---
+++ src/pandas_intro.py
@@ -0,0 +1,8 @@
+# Import numpy, pandas, and matplotlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Print the pandas version and the configuration
+print(pd.__version__)
+
