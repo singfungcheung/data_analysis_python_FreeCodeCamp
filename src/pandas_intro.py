6b987fe3f5b1a60f9d85d333829eb1b77413d33c
singfungcheung <singfung.cheung@gmail.com>
2024-04-07 13:35:37 -0400

    Created an empty data frame

--- src/pandas_intro.py
+++ src/pandas_intro.py
@@ -3,6 +3,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Print the pandas version and the configuration
-print(pd.__version__)
+# Print the pandas version and the configuration.
+# print(pd.__version__)
 
+"""
+DataFrame creation
+"""
+# Create an empty pandas DataFrame.
+# df = pd.DataFrame(data=[None],
+#                   index=[None],
+#                   columns=[None])
+# print(df)
+
+# Create a marvel_df pandas DataFrame with the given marvel data.
